59e971581f9bfd32606c2d463ca59db00c526e55
ajgru1mu <ajgru1@student.monash.edu>
2014-12-13 01:55:37 +1100

    app controller kv

--- AppController.py
+++ AppController.py
@@ -1,11 +1,30 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 
 
+class AppController(BoxLayout):
 
-class AppController(Widget):
 	student_Program = ObjectProperty(None)
 	code_View = ObjectProperty(None)
-	rune_View = ObjectProperty(None)
+	run_View = ObjectProperty(None)
 	debug_View = ObjectProperty(None)
 
-
+	def __init__(self):
+		return 
 	
+		def save_Button(student_Program):
+			pass
+			#raise(Not Implemented Error)
+
+		def load_Button():
+			pass
+			# raise(Not Implemented Error)
+
+		def run_Button():
+			pass
+			# raise(Not Implemented Error)
+
+		def debug_Button():
+			pass
+			# raise(Not Implemented Error)
