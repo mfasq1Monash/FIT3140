349359e28fae9bf278d65b0783a1c4e047dcf5aa
mfasq1Monash <mfasq1@student.monash.edu>
2014-12-13 00:37:31 +1100

    Functionlist now extends List

--- functionlist.py
+++ functionlist.py
@@ -1,13 +1,2 @@
-class FunctionList():
-
-    def __init__(self):
-        self.codeList = []
-
-    def insertInstruction(self, instruction, index):
-        self.codeList.insert(instruction, index)
-
-    def removeInstruction(self, index):
-        self.codeList.del(index)
-
-    def getInstruction(self, index):
-        return self.codeList[index]
+class FunctionList(list):
+    pass
