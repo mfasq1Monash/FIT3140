fff718a4c273f45d55d307b8735737e52a048782
ajgru1mu <ajgru1@student.monash.edu>
2014-12-13 00:05:56 +1100

    appController py
    AppController python file

---
+++ AppController.py
@@ -0,0 +1,11 @@
+
+
+
+class AppController(Widget):
+	student_Program = ObjectProperty(None)
+	code_View = ObjectProperty(None)
+	rune_View = ObjectProperty(None)
+	debug_View = ObjectProperty(None)
+
+
+	
