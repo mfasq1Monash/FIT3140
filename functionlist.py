942d73db23487f07f0410391eecdb04aef80ca25
ajgru1mu <ajgru1@student.monash.edu>
2014-12-14 09:44:27 +1100

    function list layout
    moved layout from kv file into python file

--- functionlist.py
+++ functionlist.py
@@ -1,2 +1,12 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
 class FunctionList(list):
-    pass
+
+
+	def __init__(self):
+	    layout = BoxLayout(orientation = 'vertical')
+	    layout = BoxLayout(spacing=10)
+	   for element in self:
+	   		layout.add_widget(element)
