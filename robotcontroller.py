b5cd2590c001b9386a5951273dbc47b2735fb513
mfasq1Monash <mfasq1@student.monash.edu>
2014-12-16 12:55:34 +1100

    Commented robotcontroller
    Also removed some legacy code

--- robotcontroller.py
+++ robotcontroller.py
@@ -1,20 +1,27 @@
+'''
+Author: Michael Asquith, Aaron Gruneklee
+Created: 2014.12.08
+Last Modified: 2014.12.15
+
+The brain of the robot. Has access to the robot's I/O, interpreter and
+instructions
+'''
+
+
 from interpreter import Interpreter
 from robotio import RobotIO
 from codeblock import CodeBlock
 
 class RobotController():
-    """A block with an associated function and some number of text
-    input arguments"""
+    """studentProgram is the robot's instructions"""
 
     def __init__(self, studentProgram):
         self.program = studentProgram
         self.robotio = RobotIO()
 
     def executeProgram(self):
+        """Executes the robot's program"""
         inter = Interpreter(self.robotio)
         for codeBlock in self.program:
-            Interpreter.interpret(str(codeBlock))
-
-    def numberOfParameters(self):
-        return len(self.parameters)
+            inter.interpret(str(codeBlock))
 
