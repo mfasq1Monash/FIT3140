44e4513d0f7236d0542517e3a037d99e567765b2
ajgru1mu <ajgru1@student.monash.edu>
2014-12-17 03:08:06 +1100

    added comments

--- codeblock.py
+++ codeblock.py
@@ -16,7 +16,12 @@ from kivy.uix.button import Button
 from kivy.properties import ListProperty, ObjectProperty, BooleanProperty
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from copy import copy
+from kivy.uix.widget import Widget
+
+
+
+''' the code Block is a group of primary blocks that make 
+up a block of code that can be dragged and dropped '''
 
 class CodeBlock(GridLayout):
     """A block with an associated function and some number of text
@@ -25,12 +30,15 @@ class CodeBlock(GridLayout):
 
     def __init__(self, name):
         super(CodeBlock, self).__init__()
+        layout = BoxLayout(orientation= 'horizontal')
+
         
-        code = name
-        parameters = code.replace('(', '( ').replace(')', ' )').split()
+        parameters = name.replace('(', '( ').replace(')', ' )').split()
         self.cols = len(parameters)
         for param in parameters:
-            self.add_widget(primaryBlock(param))
+            layout.add_widget(primaryBlock(param))
+
+        self.add_widget(layout)
             
 
     def on_touch_down(self, touch):
@@ -47,18 +55,24 @@ class CodeBlock(GridLayout):
             self.selected = False
 
 
-class primaryBlock(BoxLayout):
-    def __init__(self, typeValue):
-        super(BoxLayout, self).__init__()
-        self.ids.size_hint = (1,1)
-        # if str(typeValue).capitalize == 'X' or 'Y':
-        #     box = TextInput(text= 'enter value or drag code block', size_hint=(.3,1))
-        #     # box.bind(on_) need to bind to a method to accept value or a codeBlock
-        #     self.add_widget(box)
+''' a primary block is the base level for the display of the users code '''
 
-        # else:
-        box = Label(text=typeValue, size=(30,40))
-        self.add_widget(box)
+class primaryBlock(Widget):
+
+    def __init__(self, typeValue):
+        super(primaryBlock, self).__init__()
+        self.ids.size_hint = (None,None)
+        if str(typeValue).capitalize == 'X' or 'Y':
+            box = TextInput(text= 'enter value or drag code block', size_hint=(.3,1))
+            # box.bind(on_) need to bind to a method to accept value or a codeBlock
+            self.add_widget(box)
+
+        else:
+            box = Label(text=typeValue, size=(60,80))
+
+            self.add_widget(box)
+
+        
 
 
 
