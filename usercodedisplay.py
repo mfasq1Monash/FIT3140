b992e705c3010ae92533c5cab0b37da621b9b941
ajgru1mu <ajgru1@student.monash.edu>
2014-12-17 00:13:33 +1100

    header comments of user code display

---
+++ usercodedisplay.py
@@ -0,0 +1,13 @@
+'''
+Author:        Aaron Gruneklee, Michael Asquith
+Created:       2014.12.16
+Last Modified: 2014.12.16
+
+tree of code blocks to be displayed and manipulated through user interaction.
+
+this forms the display of the user code
+
+***  still need to implement drag and drop and display of draggable code blocks  ***   
+
+'''
+
