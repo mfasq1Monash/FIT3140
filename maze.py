07bc51e94008a12e574f01100febc11c8dd53cec
mfasq1Monash <mfasq1@student.monash.edu>
2014-12-16 14:49:37 +1100

    Maze isWall tests added

--- maze.py
+++ maze.py
@@ -31,10 +31,43 @@ class Maze:
     def getStartFacing(self):
         return 'South'
 
+    def isItem(self, xcoord, ycoord, item):
+        return self.grid[ycoord][xcoord] == item
+            
     def isWall(self, xcoord, ycoord):
         if( xcoord < 0 or ycoord < 0 or
             xcoord >= self.xdimension or ycoord >= self.ydimension):
             return True
-        if self.grid[ycoord][xcoord] == 1:
+        if self.isItem(xcoord, ycoord, 1):
             return True
         return False
+
+    def isGoal(self, xcoord, ycoord):
+        return isItem(xcoord, ycoord, 'G')
+
+if __name__ == '__main__':
+    maze = Maze()
+    failed_tests = []
+
+    # isWall
+    tests = [[(1,0),'Wall in maze', True],
+             [(0,0),'Corner',False],
+             [(8,8),'Corner',False],
+             [(0,-1),'Too small Y', True],
+             [(-1,0),'Too small X', True],
+             [(8,9),'Too big Y', True],
+             [(9,8),'Too big X', True]]
+
+    for i in range(len(tests)):
+        curr = tests[i]
+        result = maze.isWall(curr[0][0], curr[0][1])
+        if result != curr[2]:
+            failed_tests.append(curr)
+        print "Test ", i
+        print curr[0], "should return", curr[2], "\n", result 
+        print
+
+    if failed_tests: print "failed tests are:"
+    for i in range(len(failed_tests)):
+        print "Test ", i, " ", failed_tests[i]
+    raw_input()
