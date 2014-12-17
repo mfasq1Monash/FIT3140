2fa22dcd35694408d41fece33816637d3b91c471
mfasq1Monash <mfasq1@student.monash.edu>
2014-12-17 17:43:38 +1100

    Interpreter test AND (incomplete)

--- interpretertest.py
+++ interpretertest.py
@@ -7,26 +7,31 @@ class InterpreterTest(unittest.TestCase):
     def setUp(self):
         self.inter = Interpreter(RobotIO())
 
-    def test_evaluateboolean(self):
+    def test_evaluateBoolean(self):
         # interpreting "True" or "False" should return the boolean
         self.assertEqual(self.inter.interpret('true'), True)
         self.assertEqual(self.inter.interpret('FAlsE'), False)
 
-    def test_evaluateinteger(self):
+    def test_evaluateInteger(self):
         # interpreting an integer should return that integer
         self.assertEqual(self.inter.interpret('3'),3)
         self.assertEqual(self.inter.interpret('0'),0)
         self.assertEqual(self.inter.interpret('-500'),-500)
-        self.assertRaises(VariableNotFoundException,self.inter.interpret,'3.5')
+        self.assertRaises(VariableNotFoundException,self.inter.interpret,'3.5')        
 
-        
-
-    def test_evaluatesymbol(self):
+    def test_evaluateSymbol(self):
         # interpreting any non-integer/non-boolean should find that in the
         # interpreter's environment
         self.assertEqual(
             self.inter.interpret('pi'),
             self.inter.global_environment['pi'])
+
+    def test_and(self):
+        # boolean and
+        self.assertEqual(self.inter.interpret('(and true true)'),True)
+        self.assertEqual(self.inter.interpret('(and true false)'),False)
+        self.assertEqual(self.inter.interpret('(and false false)'),False)
+        #self.assertEqual(self.inter.interpret('(and true 3)')
         
 
 if __name__ == '__main__':
