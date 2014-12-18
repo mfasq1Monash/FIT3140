857ecbd76534b1e6f0f5b1484ee6d340946a2aa7
ajgru1mu <ajgru1@student.monash.edu>
2014-12-19 10:33:19 +1100

    started RunScreen
    still need to implement

---
+++ runscreen.py
@@ -0,0 +1,28 @@
+'''
+# Author:        Aaron Gruneklee, Michael Asquith
+# Created:       2014.12.12
+# Last Modified: 2014.12.15
+
+A display for a maze that is passed as a list of lists
+
+'''
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from robotio import RobotIO
+
+
+class RunView(GridLayout):
+	
+	def __init__(self, ):
+		pass
+
+
+
+
+class TestRunView(App):
+	def build(self):
+		return RunView()
+
+if __name__ == '__main__':
+	TestRunView().run()
