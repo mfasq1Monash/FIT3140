030fcfaa839e686eb042e677294d6bdbdfdd3c7a
ajgru1mu <ajgru1@student.monash.edu>
2014-12-20 00:27:40 +1100

    syntax fixes, and some comments

--- Drag and drop SPIKE/main.py
+++ Drag and drop SPIKE/main.py
@@ -1,4 +1,4 @@
-from kivy.app import app
+from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
@@ -6,7 +6,9 @@ class SwallowWidget(Widget):
     # contains two layouts with buttons to drag and drop
     african = ObjectProperty(None)
     for i in range(4):
-        african.addwidget(Button(str(i)))
+    	# *** african is None you need to make it a uix (layout) subclass 
+    	# to be able to add widgets *** 
+        african.add_widget(Button(str(i)))
 
 ##    european = ObjectProperty(None)
 ##    for i in range(4,8):
@@ -15,5 +17,5 @@ class SwallowWidget(Widget):
 
 
 class SwallowApp(App):
-    build(self):
+    def build(self):
         return SwallowWidget()
